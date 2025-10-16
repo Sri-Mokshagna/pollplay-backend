a26678786d4a1f9ce582eec637d8997fdac0bbf6
Jaladi Sri Mokshagna <109271984+Sri-Mokshagna@users.noreply.github.com>
2025-10-16 14:03:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import pytz
 # Set IST timezone
 IST = pytz.timezone('Asia/Kolkata')
 
-DATABASE_URL = "sqlite:///./votewise.db"
+DATABASE_URL = "sqlite:///./poll_play.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
